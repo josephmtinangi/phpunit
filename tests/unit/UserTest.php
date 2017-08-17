f19c1d497fba1fb02b6db165652d87e8c35cfd57
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-08-18 00:25:33 +0300

    Test for last_name and full_name

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -10,4 +10,22 @@ class UserTest extends \PHPUnit_Framework_TestCase
 
 		$this->assertEquals($user->getFirstName(), 'Joseph');
 	}
+
+	public function testThatWeCanGetTheLastName()
+	{
+		$user = new \App\Models\User;
+
+		$user->setLastName('Mtinangi');
+
+		$this->assertEquals($user->getLastName(), 'Mtinangi');
+	}
+
+	public function testFullNameIsReturned()
+	{
+		$user = new \App\Models\User;
+		$user->setFirstName('Joseph');
+		$user->setLastName('Mtinangi');
+
+		$this->assertEquals($user->getFullName(), 'Joseph Mtinangi');	
+	}
 }
